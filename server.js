3211dd25a8519a2618c3dd1c7a3c676f59b50d34
sicute <sonjaya@gmail.com>
2018-11-06 16:55:23 +0700

    update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello Is Working Now\n');
 });
 
 app.listen(PORT, HOST);
